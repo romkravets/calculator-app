37661254bf3414f930146f29ae7957507320fcd9
romkravets <romann.kravets@gmail.com>
2019-07-07 22:15:38 +0300

    Added change

--- js/index.js
+++ js/index.js
@@ -22,9 +22,41 @@ function equalPressed() {
    display.value = eval(display.value);
 }
 
-document.querySelector('.clear').addEventListener('click', clearAll);
+document.querySelector('.clearAll').addEventListener('click', clearAll);
 
 function clearAll() {
    document.querySelector('.display').value = '';
 }
 
+document.querySelector('.clear').addEventListener('click', clear);
+
+function clear() {
+   let targetValue = document.querySelector('.display').value;
+   if(targetValue.length > 0){
+      targetValue = targetValue.substring(0, targetValue.length - 1);
+      document.querySelector('.display').value = targetValue;
+   }
+}
+
+document.querySelector('.sqrt').addEventListener('click', sqrt);
+
+function sqrt() {
+   let sqrtDisplay = Math.sqrt(display.value);
+   sqrtDisplay.value = sqrtDisplay;
+   display.value = sqrtDisplay;
+}
+
+let saveValue
+
+function saveValueFunc() {
+   saveValue = localStorage.setItem('saveValue');
+   document.querySelector('.calculator .display').value = saveValue;
+}
+
+function getSaveValue() {
+   saveValue = document.querySelector('saveValue').value;
+   localStorage.setItem('saveValue', saveValue);
+}
+
+saveValueFunc();
+getSaveValue();
